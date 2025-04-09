cfda1e806c5847735c09d7819836aa5059cb5d56
saivenkataramireddy <saivenkataramireddykesara@gmail.com>
2025-04-09 09:23:38 +0530

    Initial commit for deployment

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,3 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets') #this is you assets folder.
 MEDIA_URL ='/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
