b5ded8ad642bc3ba879b23e6aa443fd337c75610
BhargavSellam <147702067+BhargavSellam@users.noreply.github.com>
2025-05-23 10:42:56 +0530

    Update settings.py

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'r)86=@7!1+rfy*aju_a6g9r9bhi&k@w+=&9g=yg=(fhxvt#$my'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.252.180.146','localhost']
+ALLOWED_HOSTS = ['52.64.52.241','localhost']
 
 
 # Application definition
